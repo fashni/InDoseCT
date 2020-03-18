be67ebd282100be1a6779bb0365e67f13286fd3f
Fahmi Rosdiansyah Mahdani <fashni@outlook.com>
2020-03-18 13:27:16 +0700

    change the dw unit to cm

--- IndoseCT_funcs.py
+++ IndoseCT_funcs.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import pydicom
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.measure import label, regionprops
@@ -8,13 +9,19 @@ from skimage.feature import canny
 from skimage.morphology import square, closing
 from scipy import ndimage as ndi
 
-def get_images(filepath):
+def get_images(filepath, ref=False):
   filelist = np.array(glob.glob(filepath + '/*.dcm'))
-  ref = get_reference(filelist)
+  refr = get_reference(filelist)
   print('Memuat citra...')
-  imgs = np.array([np.array(pydicom.dcmread(fname).pixel_array*ref['slope'] + ref['intercept'])
+  imgs = np.array([np.array(pydicom.dcmread(fname).pixel_array*refr['slope'] + refr['intercept'])
                   for fname in filelist])
+
   imgs.dump("citra.npy")
+  with open("reference.json", "w") as f:
+    json.dump(refr, f)
+
+  if ref:
+    return imgs, refr
   return imgs
 
 def get_reference(files):
@@ -22,9 +29,9 @@ def get_reference(files):
   ref_data = {
     "dimension": (int(ref.Rows), int(ref.Columns), len(files)),
     "spacing": (float(ref.PixelSpacing[0]), float(ref.PixelSpacing[1]), float(ref.SliceThickness)),
-    "intercept": ref[0x0028, 0x1052].value,
-    "slope": ref[0x0028, 0x1053].value,
-    "pixel_dtype": ref.pixel_array.dtype
+    "intercept": float(ref.RescaleIntercept),
+    "slope": float(ref.RescaleSlope),
+    "reconst_diameter": float(ref.ReconstructionDiameter),
   }
   return ref_data
 
@@ -46,19 +53,21 @@ def get_coord(grid, x):
   list_of_coordinates = np.array(tuple(zip(where[0], where[1])))
   return list_of_coordinates
 
-def get_dw_value(img, label):
+def get_dw_value(img, ref):
+  label = get_label(img)
+  rd = ref["reconst_diameter"]
   roi = regionprops(label.astype(int), intensity_image=img)
-  area = roi[0].area
+  area = roi[0].area*(rd*rd)/(512*512)
   avg = roi[0].mean_intensity
-  dw = 2*np.sqrt(((avg/1000)+1)*(area/np.pi))
+  dw = 0.1*2*np.sqrt(((avg/1000)+1)*(area/np.pi))
   return dw
 
-def avg_dw(imgs):
-  print('jumlah citra: {}'.format(len(imgs)))
-  dw = np.array([get_dw_value(img, get_label(img)) for img in imgs])
+def avg_dw(imgs, ref):
+  print(f'jumlah citra: {len(imgs)}')
+  dw = np.array([get_dw_value(img, ref) for img in imgs])
   count = 0
   for d in dw:
-    print('dw_{}: {}'.format(count, d))
+    print(f'dw_{count}: {d} cm')
     count+=1
   return np.mean(dw)
 
@@ -73,13 +82,15 @@ def show_imgs(img, label=None):
 
 if __name__ == "__main__":
   files_path = "D:/Undip/pelatihan/pelat TC/Citra pelatihan/Citra anthopomorphic"
-  if os.path.exists("citra.npy"):
-    dicom_pixels = np.load("citra.npy", allow_pickle=True)
-  else:
-    dicom_pixels = get_images(files_path)
 
-  avg = avg_dw(dicom_pixels)
-  print('Average Dw value: {}'.format(avg))
+  if os.path.exists("citra.npy") and os.path.exists("reference.json"):
+    dicom_pixels = np.load("citra.npy", allow_pickle=True)
+    ref = json.load(open("reference.json"))
+  else:
+    dicom_pixels, ref = get_images(files_path, True)
+
+  avg = avg_dw(dicom_pixels, ref)
+  print(f'Average Dw value: {avg} cm')
 
   citra = dicom_pixels[12]
   show_imgs(citra, get_label(citra))
