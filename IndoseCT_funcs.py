bdcc02b2c464ffbcc132613237f84416b32daa81
Fahmi Rosdiansyah Mahdani <fashni@outlook.com>
2020-06-17 20:26:44 +0700

    add effective diameter calculation

--- IndoseCT_funcs.py
+++ IndoseCT_funcs.py
@@ -59,23 +59,66 @@ def get_coord(grid, x):
   list_of_coordinates = np.array(tuple(zip(where[0], where[1])))
   return list_of_coordinates
 
+def get_px_area(roi):
+  return roi[0].area
+
+def get_avg_intensity(roi):
+  return roi[0].mean_intensity
+
+def get_roi(img, label):
+  return regionprops(label.astype(int), intensity_image=img)
+
 def get_dw_value(img, ref):
   label = get_label(img)
   rd = ref["reconst_diameter"]
-  roi = regionprops(label.astype(int), intensity_image=img)
-  area = roi[0].area*(rd**2)/(len(img)**2)
-  avg = roi[0].mean_intensity
+  roi = get_roi(img, label)
+  px_area = get_px_area(roi)
+  area = px_area*(rd**2)/(len(img)**2)
+  avg = get_avg_intensity(roi)
   dw = 0.1*2*np.sqrt(((avg/1000)+1)*(area/np.pi))
   return dw
 
-def avg_dw(imgs, ref):
-  print(f'jumlah citra: {len(imgs)}')
-  dw = np.array([get_dw_value(img, ref) for img in imgs])
-  count = 0
-  for d in dw:
-    print(f'dw_{count}: {d} cm')
-    count+=1
-  return np.mean(dw)
+def get_deff_value(img, ref, method):
+  label = get_label(img)
+  rd = ref["reconst_diameter"]
+  roi = get_roi(img, label)
+  px_area = get_px_area(roi)
+  if method == 'area':
+    area = px_area*(rd**2)/(len(img)**2)
+    deff = 2*0.1*np.sqrt(area/np.pi)
+    cen_row = None
+    cen_col = None
+  elif method == 'center':
+    pos = get_coord(label, True)
+    N = len(pos)
+    xpos = sum(pos[:, 0]) + 1
+    ypos = sum(pos[:, 1]) + 1
+    cen_row = int(np.round(xpos/N))
+    cen_col = int(np.round(ypos/N))
+
+    row, col = label.shape
+    nrow1 = sum(label[cen_row, :])
+    ncol1 = sum(label[:, cen_col])
+
+    len_row = nrow1 * (0.1*rd/row)
+    len_col = ncol1 * (0.1*rd/col)
+
+    deff = np.sqrt(len_row*len_col)
+  elif method == 'max':
+    row, col = label.shape
+    len_rows = [sum(label[r, :] for r in range(row))]
+    len_cols = [sum(label[:, c] for c in range(col))]
+
+    len_row = np.max(len_rows) * (0.1*rd/row)
+    len_col = np.max(len_cols) * (0.1*rd/col)
+
+    cen_row = np.argmax(len_rows)
+    cen_col = np.argmax(len_cols)
+
+    deff = np.sqrt(len_row*len_col)
+  else:
+    deff = 0
+  return deff, cen_row, cen_col
 
 def get_label_pos(label):
   edges = canny(label)
@@ -106,7 +149,14 @@ if __name__ == "__main__":
   filelist = np.array(filedialog.askopenfilenames())
   dicom_pixels, ref, pat_info = get_images(filelist, True)
   root.destroy()
-  print(pat_info)
+  area, _, _ = get_deff_value(dicom_pixels[0], ref, 'area')
+  center, _, _ = get_deff_value(dicom_pixels[0], ref, 'center')
+  _max, _, _ = get_deff_value(dicom_pixels[0], ref, 'max')
+  dw = get_dw_value(dicom_pixels[0], ref)
+  print(f'deff area = {area: #.2f} cm')
+  print(f'deff center = {center: #.2f} cm')
+  print(f'deff max = {_max: #.2f} cm')
+  print(f'dw = {dw: #.2f} cm')
 
   # avg = avg_dw(dicom_pixels, ref)
   # print(f'Average Dw value: {avg} cm')
--- tab_Diameter.py
+++ tab_Diameter.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QLineEdit, QPushButton, QScrollArea, QRadioButton, QButtonGroup
 from PyQt5.QtCore import Qt
 from tab_CTDIvol import GetMainWindowProps
-from IndoseCT_funcs import get_dw_value, avg_dw, get_label
+from IndoseCT_funcs import get_dw_value, get_deff_value, get_label
 from separator import VSeparator
 import sip
 
@@ -13,6 +13,8 @@ class DiameterTab(QWidget):
 
   def initVar(self):
     self.d_val = 0
+    self.def_auto_method = 'area'
+    self._3d_method = 'slice step'
     self.src_method = {
       'Get from Image': ['Auto', 'Auto (3D)', 'Manual'],
       'Input Manually': ['Manual'],
@@ -45,7 +47,7 @@ class DiameterTab(QWidget):
     self.method_cb.setCurrentIndex(-1)
 
     self.calc_btn = QPushButton('Calculate')
-    self.calc_btn.clicked.connect(self.calculate)
+    self.calc_btn.clicked.connect(self._calculate)
     d_lbl = QLabel('Diameter')
     unit = QLabel('cm')
     self.d_out = QLineEdit('0')
@@ -98,7 +100,7 @@ class DiameterTab(QWidget):
     self.btn_grp.addButton(opt1)
     self.btn_grp.addButton(opt2)
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('Options'))
+    inner.addWidget(QLabel('Options:'))
     inner.addWidget(opt1)
     inner.addWidget(opt2)
     inner.addStretch()
@@ -106,29 +108,37 @@ class DiameterTab(QWidget):
 
   def _ui_dw_img_3d(self):
     opt1 = QRadioButton('Slice Step')
-    opt1.setChecked(True)
     opt2 = QRadioButton('Slice Number')
+    self.slices = QLineEdit('1')
+    self.slices.setMaximumWidth(50)
     self.btn_grp = QButtonGroup()
     self.btn_grp.addButton(opt1)
     self.btn_grp.addButton(opt2)
+    opt1.toggled.connect(self._3d_switch)
+    opt2.toggled.connect(self._3d_switch)
+    opt1.setChecked(True)
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('3D Options'))
+    inner.addWidget(QLabel('3D Options:'))
     inner.addWidget(opt1)
     inner.addWidget(opt2)
+    inner.addWidget(self.slices)
     inner.addStretch()
     self.opt.setLayout(inner)
 
   def _ui_def_img_auto(self):
     opt1 = QRadioButton('Area')
-    opt1.setChecked(True)
     opt2 = QRadioButton('Center')
     opt3 = QRadioButton('Max')
     self.btn_grp = QButtonGroup()
     self.btn_grp.addButton(opt1)
     self.btn_grp.addButton(opt2)
     self.btn_grp.addButton(opt3)
+    opt1.toggled.connect(self._def_auto_switch)
+    opt2.toggled.connect(self._def_auto_switch)
+    opt3.toggled.connect(self._def_auto_switch)
+    opt1.setChecked(True)
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('Options'))
+    inner.addWidget(QLabel('Options:'))
     inner.addWidget(opt1)
     inner.addWidget(opt2)
     inner.addWidget(opt3)
@@ -141,6 +151,8 @@ class DiameterTab(QWidget):
     base3 = QRadioButton('Max')
     _3d1 = QRadioButton('Slice Step')
     _3d2 = QRadioButton('Slice Number')
+    self.slices = QLineEdit('1')
+    self.slices.setMaximumWidth(50)
     self.base_grp = QButtonGroup()
     self._3d_grp = QButtonGroup()
     self.base_grp.addButton(base1)
@@ -148,22 +160,23 @@ class DiameterTab(QWidget):
     self.base_grp.addButton(base3)
     self._3d_grp.addButton(_3d1)
     self._3d_grp.addButton(_3d2)
-    base1.setChecked(True)
-    _3d1.setChecked(True)
     _3d1.toggled.connect(self._3d_switch)
     _3d2.toggled.connect(self._3d_switch)
     base1.toggled.connect(self._def_auto_switch)
     base2.toggled.connect(self._def_auto_switch)
     base3.toggled.connect(self._def_auto_switch)
+    base1.setChecked(True)
+    _3d1.setChecked(True)
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('Options'))
+    inner.addWidget(QLabel('Options:'))
     inner.addWidget(base1)
     inner.addWidget(base2)
     inner.addWidget(base3)
     inner.addWidget(QLabel(''))
-    inner.addWidget(QLabel('3D Options'))
+    inner.addWidget(QLabel('3D Options:'))
     inner.addWidget(_3d1)
     inner.addWidget(_3d2)
+    inner.addWidget(self.slices)
     inner.addStretch()
     self.opt.setLayout(inner)
 
@@ -172,7 +185,7 @@ class DiameterTab(QWidget):
     opt1.setChecked(True)
     opt2 = QPushButton('AP')
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('Options'))
+    inner.addWidget(QLabel('Options:'))
     inner.addWidget(opt1)
     inner.addWidget(opt2)
     inner.addStretch()
@@ -192,7 +205,7 @@ class DiameterTab(QWidget):
     btn_grp.addButton(opt4)
     btn_grp.addButton(opt5)
     inner = QVBoxLayout()
-    inner.addWidget(QLabel('Options'))
+    inner.addWidget(QLabel('Options:'))
     inner.addWidget(opt1)
     inner.addWidget(opt2)
     inner.addWidget(opt3)
@@ -237,21 +250,73 @@ class DiameterTab(QWidget):
   def _3d_switch(self):
     sel = self.sender()
     if sel.isChecked():
-      print(sel.text())
+      self._3d_method = sel.text().lower()
+      print(self._3d_method)
 
   def _def_auto_switch(self):
     sel = self.sender()
     if sel.isChecked():
-      print(sel.text())
+      self.def_auto_method = sel.text().lower()
+      print(self.def_auto_method)
 
-  def calculate(self):
+  def _calculate(self):
+    self._params
+    if self.source == 0: # from img
+      if self.method == 0: # auto
+        self._auto()
+      elif self.method == 1: # 3d
+        self._auto_3d()
+      elif self.method == 2: # manual
+        self._img_manual()
+    elif self.source == 1: # input manual
+      self._input_manual()
+
+  def _auto(self):
     try:
       with GetMainWindowProps(self) as par:
-        img = par.imgs[par.current_img-1]
         info = par.first_info
+        img = par.imgs[par.current_img-1]
         img_label = get_label(img)
+        par.axes.clear()
         par.axes.imshow(img, img_label)
     except:
       return
-    self.d_val = get_dw_value(img, info)
+    if self.based_on == 0: # deff
+      self.d_val, x, y = get_deff_value(img, info, self.def_auto_method)
+    elif self.based_on == 1:
+      self.d_val = get_dw_value(img, info)
     self.d_out.setText(f'{self.d_val:#.2f}')
+    
+  def _auto_3d(self):
+    try:
+      nslice = int(self.slices.text())
+    except:
+      return
+
+    try:
+      with GetMainWindowProps(self) as par:
+        info = par.first_info
+        imgs = par.imgs
+        dval = 0
+        if self._3d_method == 'slice step':
+          n = len(imgs[::nslice])
+          print(n)
+          for idx, img in enumerate(imgs[::nslice]):
+            if self.based_on == 0:
+              d, _, _ = get_deff_value(img, info, self.def_auto_method)
+            else:
+              d = get_dw_value(img, info)
+            dval += d
+            par.progress.setValue((idx+1)*100/n)
+          self.d_val = dval/n
+        elif self._3d_method == 'slice number':
+          pass
+        self.d_out.setText(f'{self.d_val:#.2f}')
+    except:
+      return
+
+  def _img_manual(self):
+    pass
+
+  def _input_manual(self):
+    pass
